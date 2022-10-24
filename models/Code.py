650fe6f3c65e4362411c2b1c08541002a067617f
Scot_Survivor <unconfigured@null.spigotmc.org>
2022-10-24 10:01:43 +0100

    Fix null constraint bug

--- models/Code.py
+++ models/Code.py
@@ -15,8 +15,8 @@ class Code(Base):
     updated_at = Column(DateTime, nullable=False, default=datetime.datetime.now)
 
     module_id = Column(Integer, ForeignKey('modules.id'), nullable=False)
-    lecture_id = Column(Integer, ForeignKey('lectures.id'), nullable=False)
-    seminar_id = Column(Integer, ForeignKey('seminars.id'), nullable=False)
+    lecture_id = Column(Integer, ForeignKey('lectures.id'), nullable=True)
+    seminar_id = Column(Integer, ForeignKey('seminars.id'), nullable=True)
 
     def __repr__(self):
         return f"Module(name='{self.name}', description='{self.description}', status={self.status}, created_at='{self.created_at}', updated_at='{self.updated_at}')"
