6b62c710e0b920ac4ff9eecb4f909139cdb08e05
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2022-10-27 13:46:02 +0100

    Check if code already exists logic

--- ui_components/AddCode.py
+++ ui_components/AddCode.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 
 class CodeDropdown(nextcord.ui.Select):
-    def __init__(self, code: str, engine, module: Module, seminars: typing.List[Seminar] = None, lectures: typing.List[Lecture] = None ):
+    def __init__(self, code: str, engine, module: Module, seminars: typing.List[Seminar] = None, lectures: typing.List[Lecture] = None):
         if seminars is None and lectures is None:
             raise ValueError("You must pass either seminars or lectures")
         self.seminars = seminars
@@ -28,23 +28,31 @@ class CodeDropdown(nextcord.ui.Select):
 
         super(CodeDropdown, self).__init__(placeholder="Select a seminar/lecture", min_values=1, max_values=1, options=options)
 
+    def check_if_code_exists(self, session: Session):
+        stmt = select(Code).where(Code.code == self.code)
+        return session.execute(stmt).scalars().first() is not None
+
     async def callback(self, interaction: nextcord.Interaction):
         with Session(self.engine) as session:
-            stmt = select(Seminar).where(Seminar.id == self.values[0])
-            obj_seminar = session.execute(stmt).scalars().first()
-            stmt = select(Lecture).where(Lecture.name == self.values[0])
-            obj_lecture = session.execute(stmt).scalars().first()
-            if obj_seminar is None and obj_lecture is None:
-                await interaction.response.send_message("Seminar or lecture does not exist. Please ask an admin to create it")
+            if not self.check_if_code_exists(session):
+                stmt = select(Seminar).where(Seminar.id == self.values[0])
+                obj_seminar = session.execute(stmt).scalars().first()
+                stmt = select(Lecture).where(Lecture.name == self.values[0])
+                obj_lecture = session.execute(stmt).scalars().first()
+                if obj_seminar is None and obj_lecture is None:
+                    await interaction.response.send_message("Seminar or lecture does not exist. Please ask an admin to create it")
+                    return
+
+                if obj_seminar is not None:
+                    obj_code = Code(code=self.code, module_id=self.module.id, seminar_id=obj_seminar.id)
+                elif obj_lecture is not None:
+                    obj_code = Code(code=self.code, module_id=self.module.id, lecture_id=obj_lecture.id)
+
+                session.add(obj_code)
+                session.commit()
+            else:
+                await interaction.response.send_message("Code already exists")
                 return
-
-            if obj_seminar is not None:
-                obj_code = Code(code=self.code, module_id=self.module.id, seminar_id=obj_seminar.id)
-            elif obj_lecture is not None:
-                obj_code = Code(code=self.code, module_id=self.module.id, lecture_id=obj_lecture.id)
-
-            session.add(obj_code)
-            session.commit()
 
         await interaction.response.send_message(f"Added code! {self.code} for {self.module.name}")
 
